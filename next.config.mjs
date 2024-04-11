fa7d84a3282f918938b5e98fc822631307163e22
Tony <only_liru@sina.com>
2024-04-11 19:58:04 +0800

    rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
+        destination: `https://papi.unemeta.com/:path*`,
       },
     ];
   },

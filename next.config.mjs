4922886b566857464e7c7737f49610ead634e7de
Tony <only_liru@sina.com>
2024-04-11 20:14:46 +0800

    prod env

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,13 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `https://papi.unemeta.com/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },

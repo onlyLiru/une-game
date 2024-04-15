3a5e0d1ef0ae1c8f621dd24c3e8ed54f07b285ca
Tony <only_liru@sina.com>
2024-04-15 15:20:35 +0800

    classname

--- src/app/[locale]/events/game/Setting.tsx
+++ src/app/[locale]/events/game/Setting.tsx
@@ -3,13 +3,16 @@ import { Image } from "@chakra-ui/react";
 
 function Setting() {
   return (
-    <Image
-      className="flex-1"
-      src="https://unemeta-1322481783.cos.ap-tokyo.myqcloud.com/events%2Fgame%2F20240415-145913.png"
-      w="3rem"
-      h="auto"
-      alt="login"
-    />
+    <div className="relative">
+      <Image
+        className="flex-1"
+        src="https://unemeta-1322481783.cos.ap-tokyo.myqcloud.com/events%2Fgame%2F20240415-145913.png"
+        w="3rem"
+        h="auto"
+        alt="login"
+      />
+      <div className="absolute">asdfs</div>
+    </div>
   );
 }
 
